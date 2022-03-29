1b7077f4517bd4b6ce3ffea431688865f9c320cf
Crowdbotics <team@crowdbotics.com>
2022-03-29 18:42:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shower_pro_34494",
+  url: "https://shower_pro_34494.botics.co",
+  api: "https://shower_pro_34494.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
